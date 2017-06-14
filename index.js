da480554be47902c8fc12502be22436c6bb6cd55
Mac Heller-Ogden <mac@machellerogden.com>
2017-06-14 13:50:31 -0500

    update

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 'use strict';
 
 const os = require('os');
-const Promise = require('bluebird');
 const _ = require('lodash');
 
 function ip() {
     const ip = _.result(_.find(_.result(os.networkInterfaces(), 'en0', []), { family: 'IPv4' }), 'address', 'unknown');
-    return Promise.resolve(ip);
+    return ip;
 }
 
 ip.label = 'Public IP';
 
 module.exports = {
-    'gimie.ip': ip
+    ip
 };
